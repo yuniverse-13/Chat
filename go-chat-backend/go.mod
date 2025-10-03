fb05d05360a1293d84e714f9fd75f8f58a76a8b7
Timur Yun <timur.yun.13@gmail.com>
2025-10-04 00:01:11 +0500

    Containerize full application stack

--- go-chat-backend/go.mod
+++ go-chat-backend/go.mod
@@ -2,4 +2,12 @@ module my-chat-app/backend
 
 go 1.24.0
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/nats-io/nats.go v1.46.1 // indirect
+	github.com/nats-io/nkeys v0.4.11 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+)
--- go-chat-backend/main.go
+++ go-chat-backend/main.go
@@ -3,8 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/nats-io/nats.go"
+)
+
+const (
+	natsURL      = "nats://nats:4222"
+	chatSubject  = "chat.global"
 )
 
 type Client struct {
@@ -18,14 +25,16 @@ type Hub struct {
 	broadcast  chan []byte
 	register   chan *Client
 	unregister chan *Client
+	natsConn   *nats.Conn
 }
 
-func newHub() *Hub {
+func newHub(nc *nats.Conn) *Hub {
 	return &Hub{
 		broadcast:  make(chan []byte),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
+		natsConn:   nc,
 	}
 }
 
@@ -34,13 +43,13 @@ func (h *Hub) run() {
 		select {
 		case client := <-h.register:
 			h.clients[client] = true
-			log.Println("Новый клиент зарегистрирован")
+			log.Println("Новый клиент зарегистрирован в хабе")
 
 		case client := <-h.unregister:
 			if _, ok := h.clients[client]; ok {
 				delete(h.clients, client)
 				close(client.send)
-				log.Println("Клиент отключился")
+				log.Println("Клиент отключен от хаба")
 			}
 
 		case message := <-h.broadcast:
@@ -62,21 +71,6 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
-	client.hub.register <- client
-
-	log.Println("Клиент успешно подключился!")
-
-	go client.writePump()
-	go client.readPump()
-}
-
 func (c *Client) readPump() {
 	defer func() {
 		c.hub.unregister <- c
@@ -90,15 +84,17 @@ func (c *Client) readPump() {
 			}
 			break
 		}
-		log.Printf("Получено сообщение от клиента: %s", message)
-		c.hub.broadcast <- message
+		err = c.hub.natsConn.Publish(chatSubject, message)
+		if err != nil {
+			log.Printf("Ошибка публикации в NATS: %v", err)
+		} else {
+			log.Printf("Сообщение опубликовано в NATS: %s", message)
+		}
 	}
 }
 
 func (c *Client) writePump() {
-	defer func() {
-		c.conn.Close()
-	}()
+	defer c.conn.Close()
 
 	for message := range c.send {
 		w, err := c.conn.NextWriter(websocket.TextMessage)
@@ -115,20 +111,50 @@ func (c *Client) writePump() {
 		}
 	}
 
-	c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+	_ = c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+}
+
+
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	log.Println("Клиент успешно подключен по WebSocket")
+
+	go client.writePump()
+	go client.readPump()
 }
 
 func main() {
-	hub := newHub()
+	nc, err := nats.Connect(natsURL, nats.Timeout(5*time.Second))
+	if err != nil {
+		log.Fatalf("Не удалось подключиться к NATS: %v", err)
+	}
+	defer nc.Close()
+	log.Println("Успешно подключено к NATS на", natsURL)
+
+	hub := newHub(nc)
 	go hub.run()
+
+	_, err = nc.Subscribe(chatSubject, func(msg *nats.Msg) {
+		log.Printf("Получено сообщение из NATS: %s", msg.Data)
+		hub.broadcast <- msg.Data
+	})
+	if err != nil {
+		log.Fatalf("Не удалось подписаться на тему NATS: %v", err)
+	}
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
 
 	log.Println("HTTP-сервер запущен на http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
